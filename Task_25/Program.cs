f287cb5377d976b9f7f3a95b4b73d6b8bc6716cd
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-10-02 16:53:07 +0300

    Task_27 finish

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -2,16 +2,16 @@
 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 */
 
-  Console.Write("Введите число   : ");
-  int num = int.Parse(Console.ReadLine());
-  Console.Write("Введите степень : ");
-  int degree = int.Parse(Console.ReadLine());
-  int num_degree=1;
-  for(int i=0; i<degree; i++) 
-  {
-   num_degree*=num;
-  }
-  Console.WriteLine("{0} ^ {1} = {2}", num, degree, num_degree);
-  //Console.WriteLine($"Ваше число ввозведенное в степень:  " +  num_degree);
-  Console.ReadLine();
-  return 0;
+Console.Write("Введите число   : ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите степень : ");
+int degree = int.Parse(Console.ReadLine());
+int num_degree = 1;
+for (int i = 0; i < degree; i++)
+{
+    num_degree *= num;
+}
+Console.WriteLine("{0} ^ {1} = {2}", num, degree, num_degree);
+//Console.WriteLine($"Ваше число возведенное в степень:  " +  num_degree);
+Console.ReadLine();
+return 0;
--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -4,3 +4,13 @@
 82 -> 10
 9012 -> 12
 */
+
+Console.Write("Введите число   : ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма цифр:  " + sum);
