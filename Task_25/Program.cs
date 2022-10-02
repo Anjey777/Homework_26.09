2c269a420a46c75e81e752a60f79b02dbeaeb3d0
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-10-02 16:31:23 +0300

    Nfsk_25 finish

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,37 +1,17 @@
 ﻿/*
 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-3, 5 -> 243 (3⁵)
-2, 4 -> 16
-*/
-/*int degree (int numberA, int numberB)
-{
-    int result = 1;
-    for (int i = 1; i <= numberB; i++)
-    {
-        result = result * numberA;
-    }
-    return result;
-}
-
-Console.Write("Введите число A: ");
-int numberA = int.Parse(Console.ReadLine());
-Console.Write("Введите число B: ");
-int numberB = int.Parse(Console.ReadLine());
-
-int degree = degree(numberA, numberB);
-Console.WriteLine("Ответ: " + degree);
 */
 
-
-Console.Write("Введите число A: ");
-int numberA = int.Parse(Console.ReadLine());
-Console.Write("Введите число B: ");
-int numberB = int.Parse(Console.ReadLine());
-
-int degree(int umberA, int numberB)
-{
-   if (numberB==0) return 1;
-   if (numberA==1) return numberB;
-   return degree(numberA, numberB/2)*degree(numberB, numberA - numberA/2);
-}
-Console.WriteLine($"Расстояние между точками: {degree}");
+  Console.Write("Введите число   : ");
+  int num = int.Parse(Console.ReadLine());
+  Console.Write("Введите степень : ");
+  int degree = int.Parse(Console.ReadLine());
+  int num_degree=1;
+  for(int i=0; i<degree; i++) 
+  {
+   num_degree*=num;
+  }
+  Console.WriteLine("{0} ^ {1} = {2}", num, degree, num_degree);
+  //Console.WriteLine($"Ваше число ввозведенное в степень:  " +  num_degree);
+  Console.ReadLine();
+  return 0;
